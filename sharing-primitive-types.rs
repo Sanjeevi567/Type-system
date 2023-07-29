66cbcf4a103b425fd1e73efeeb21cbbac47a61d3
Sanjeevi <sanjeevimani567@gmail.com>
2023-07-29 09:46:05 +0530

    Create sharing-primitive-types.rs

---
+++ sharing-primitive-types.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use std::thread::spawn;
+fn main() {
+    let Integer: u64 = 4560;
+    let mut string: String = String::from("Not thread safe");
+    let mut hashmap: HashMap<&str, Vec<i32>> = HashMap::new();
+    hashmap.insert("Key", vec![90, 456]);
+    spawn(|| {
+        //We can't reference a variable outside of closure
+        println!("{}", Integer);
+        //nor mutate them inside closure
+        string.push_str("won't compile");
+    });
+    for i in 0..10 {
+        //try to add 1 to loop local variable in
+        //different threads
+        spawn(|| i + 1);
+    }
+
+    //Mutatating string/hashmap in different threads
+    //without any synchronization
+    spawn(|| {
+        string.push_str("Some thread");
+        hashmap.insert("Spawned", vec![45, 23]);
+    });
+    spawn(|| {
+        string.push_str("Some other thread");
+        hashmap.insert("Other spawned", vec![51, 48]);
+    });
+}
