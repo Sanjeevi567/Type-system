140f3c9f87c00babeb9a73cd67b024aa6a688129
Sanjeevi <sanjeevimani567@gmail.com>
2023-07-29 21:28:50 +0530

    Update pattern-matching.rs

--- pattern-matching.rs
+++ pattern-matching.rs
@@ -1,1 +1,10 @@
-
+Pattern matching chapter
+catch all patter 
+_ ignore single field
+.. ignore multiple remaining fields or variants after using required fields
+@
+.. ranges inclusive
+| -Or is there and ,not pattern available
+Must match return type 
+contain block of expression (calll function,refer local & capture environment if the scope is not inside of function
+if , * ,& ,ref ,ref mut ,                             
