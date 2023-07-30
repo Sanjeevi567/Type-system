d47abeb82901e3be85f3918fecdb4900c42ef68c
Sanjeevi <sanjeevimani567@gmail.com>
2023-07-30 21:31:54 +0530

    Create spawn-bound-magic.rs

---
+++ spawn-bound-magic.rs
@@ -0,0 +1,47 @@
+use std::thread::spawn;
+fn main() {
+    //Inside the closure the m is independent of value
+    let copy_type = 10;
+
+    //Even though m cloned inside the closure ,
+    //But m1 lifetime depend on the m
+    //Rust compiler not that smart enough to make m1 liftime is tied to clone of m inside of closure
+    //So this is compile error but the error not say anything about this variable.
+    //The code will compile if any reference doesn't depend on the above i32 value i.e in this case this &i32
+    let short_lifetime: &i32 = &copy_type;
+
+    //This reference has static lifetime
+    let static_ref: &'static i32 = &89;
+
+    let owned_data = String::from("Owned data are Moved");
+
+    spawn(move || {
+        println!(
+            "We are in spawned thread: {copy_type}\n{static_ref}\n{short_lifetime}\n{owned_data}"
+        );
+    })
+    .join();
+
+    //Copy types are cloned when Moved
+    //So we can use after the closure
+    println!("We are in main thread: {copy_type}\n{static_ref} ");
+    //But Move types are moved inside the closure
+    //Thus we can't use here once passed to closure
+    //println!("{owned_data}");
+
+    //  hello();
+    // println!("{HELLO}");
+    //Box::leak(static);
+
+    let a = 10;
+    let b = &a; //'a
+    let b = &a; //'b
+    let c: &&i32 = &b; // Reference of Reference i32
+}
+
+fn hello() {
+    //Only refer inside function body
+    //but data in binary
+    static HELLO: i32 = 10;
+    println!("{HELLO}");
+}
